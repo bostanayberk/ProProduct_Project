385af349b53e153213f63e8f5f22fd327f9b5cec
Ayberk BOSTAN <Ayberk BOSTAN@DESKTOP-0AI8IGL>
2025-01-19 16:10:58 +0300

    Product class(model) open  -weatherforecastcontroller deleted

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Product_Project_Api.Models
+{
+    public class Product
+    {
+        /// <summary>
+        ///ürün Id'si (Primary key)
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// ürün adi
+        /// </summary>
+        public string ProductName { get; set; }
+        /// <summary>
+        /// ürün fiyati
+        /// </summary>
+        public decimal Price { get; set; }
+    }
+}
